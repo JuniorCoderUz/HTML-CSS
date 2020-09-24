2cc0ec60ce2f790813a499ba5b46b7d3069c7093
JuniorCoderUz <juniorcoder.uz@gmail.com>
2020-09-24 13:18:56 +0500

    test1.2

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,16 @@
 'use strict';
-const test = document.querySelector('h1');
+const test = document.querySelector('h1'),
+  joke = document.querySelector('a');
+
+
+joke.addEventListener('click', e => {
+  e.preventDefault();
+  console.log("hello");
+  joke.textContent = "Go to fuck🤬";
+
+});
+
+// function darkNetJoke(e) {
+//   e.preventDefault();
+//   e.textContent = "Go to Fuck haha";
+// }
