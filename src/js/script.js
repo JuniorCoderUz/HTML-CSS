a442394850605ad4372801fbdf9bdbf8efc35981
JuniorCoderUz <juniorcoder.uz@gmail.com>
2020-10-02 23:05:51 +0500

    lesson1.5|017 Практика. Создаем сайт на чистом HTML и CSS

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,1 @@
 'use strict';
-const test = document.querySelector('h1'),
-  joke = document.querySelector('a');
-
-
-joke.addEventListener('click', e => {
-  e.preventDefault();
-  console.log("hello");
-  joke.textContent = "Go to fuck🤬";
-
-});
-
-// function darkNetJoke(e) {
-//   e.preventDefault();
-//   e.textContent = "Go to Fuck haha";
-// }
